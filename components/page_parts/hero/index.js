82a30493c399b055f6f6e98deadfba7fc2493579
AugustasPaliulis <Augustas.paliulis@outlook.com>
2022-12-29 17:31:42 +0200

    Hero mobile with flex

--- components/page_parts/hero/index.js
+++ components/page_parts/hero/index.js
@@ -4,47 +4,51 @@ import Avatar from "../../../styles/avatars/Avatar #14.svg";
 import Button from "../../button";
 import RocketLaunch from "../../../styles/icons/RocketLaunch.svg";
 
+import React from "react";
+
 const Hero = () => {
   return (
-    <div className={styles.hero_container}>
-      <div className={styles.hero_info_container}>
-        <h1>Discover digital art & Collect NFTs</h1>
-        <h5>
-          NFT marketplace UI created with Anima for Figma. Collect, buy and sell
-          art from more than 20k NFT artists.
-        </h5>
-        <Button>
-          <RocketLaunch className="mg-r-10" />
-          Get Started
-        </Button>
-        <div className={styles.hero_info_ratings}>
-          <ul>
-            <li>
-              <h4>240k+</h4>Total Sales
-            </li>
-            <li>
-              <h4>100k+</h4>Auctions
-            </li>
-            <li>
-              <h4>240k+</h4>Artists
-            </li>
-          </ul>
+    <>
+      <div className={`${styles.hero_container}`}>
+        <div className={styles.hero_info_container}>
+          <h1>Discover digital art & Collect NFTs</h1>
+          <h5>
+            NFT marketplace UI created with Anima for Figma. Collect, buy and
+            sell art from more than 20k NFT artists.
+          </h5>
+          <Button>
+            <RocketLaunch className="mg-r-10" />
+            Get Started
+          </Button>
+          <div className={styles.hero_info_ratings}>
+            <ul>
+              <li>
+                <h4>240k+</h4>Total Sales
+              </li>
+              <li>
+                <h4>100k+</h4>Auctions
+              </li>
+              <li>
+                <h4>240k+</h4>Artists
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      {/* IMAGE OF HERO */}
-      <div className={styles.hero_image_container}>
-        <div className={styles.hero_image_card}>
-          <HeroImage />
-          <div className={styles.hero_image_card_bottom}>
-            <h5>Space Walking</h5>
-            <div className={styles.hero_image_author}>
-              <Avatar className="mg-r-10" width="24px" height="24px" />
-              AnimaKid
+        {/* IMAGE OF HERO */}
+        <div className={styles.hero_image_container}>
+          <div className={styles.hero_image_card}>
+            <HeroImage />
+            <div className={styles.hero_image_card_bottom}>
+              <h5>Space Walking</h5>
+              <div className={styles.hero_image_author}>
+                <Avatar className="mg-r-10" width="24px" height="24px" />
+                AnimaKid
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
