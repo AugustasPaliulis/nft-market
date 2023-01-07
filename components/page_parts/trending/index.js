08925a91ee35567ffb429d7e279e1363ab89dbe5
AugustasPaliulis <Augustas.paliulis@outlook.com>
2023-01-07 13:25:14 +0200

    collection NFT popup modal

--- components/page_parts/trending/index.js
+++ components/page_parts/trending/index.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./trending.module.css";
 
 import Collection from "../../collection";
+import NftModal from "../../nft_modal";
 
 const Trending = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalPicture, setModalPicture] = useState();
+
   return (
-    <div className={styles.trending}>
-      <div className={styles.title_container}>
-        <h3>Trending Collection</h3>
-        <h5>Checkout our weekly updated trending collection.</h5>
+    <>
+      <div className={styles.trending}>
+        <div className={styles.title_container}>
+          <h3>Trending Collection</h3>
+          <h5>Checkout our weekly updated trending collection.</h5>
+        </div>
+        <div className={styles.collections_container}>
+          <div className={styles.seperate_collection}>
+            <Collection
+              setModalPicture={setModalPicture}
+              showModal={setShowModal}
+              type="animals"
+            />
+          </div>
+          <div className={styles.seperate_collection}>
+            <Collection
+              setModalPicture={setModalPicture}
+              showModal={setShowModal}
+              type="mushrooms"
+            />
+          </div>
+          <div className={styles.seperate_collection}>
+            <Collection
+              setModalPicture={setModalPicture}
+              showModal={setShowModal}
+              type="disco"
+            />
+          </div>
+        </div>
       </div>
-      <div className={styles.collections_container}>
-        <div className={styles.seperate_collection}>
-          <Collection type="animals" />
-        </div>
-        <div className={styles.seperate_collection}>
-          <Collection type="mushrooms" />
-        </div>
-        <div className={styles.seperate_collection}>
-          <Collection type="disco" />
-        </div>
-      </div>
-    </div>
+      <NftModal picture={modalPicture} open={showModal} close={setShowModal} />
+    </>
   );
 };
 
