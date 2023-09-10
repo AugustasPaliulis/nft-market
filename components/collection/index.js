a999220ad53d7fe0a9d41857deaabc1ce81f32e5
AugustasPaliulis <Augustas.paliulis@outlook.com>
2023-09-10 22:28:39 +0200

    nft collection page

--- components/collection/index.js
+++ components/collection/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
 
 import styles from "./collection.module.css";
 import NftModal from "../nft_modal";
@@ -23,27 +23,27 @@ import Disco3 from "../../styles/images/disco_collection/third_disco.svg";
 const collections = [
   {
     type: "animals",
-    0: <Dog id="#0"/>,
-    1: <Cat id="#1"/>,
-    2: <Bear id="#2"/>,
+    0: <Dog id="#0" />,
+    1: <Cat id="#1" />,
+    2: <Bear id="#2" />,
     title: "DSGN Animals",
     authorIcon: <MrFox />,
     authorName: "MrFox",
   },
   {
     type: "mushrooms",
-    0: <Mushroom1 id="#3"/>,
-    1: <Mushroom2 id="#4"/>,
-    2: <Mushroom3 id="#5"/>,
+    0: <Mushroom1 id="#3" />,
+    1: <Mushroom2 id="#4" />,
+    2: <Mushroom3 id="#5" />,
     title: "Magic Mushrooms",
     authorIcon: <Shroomie />,
     authorName: "Shroomie",
   },
   {
     type: "disco",
-    0: <Disco1 id="#6"/>,
-    1: <Disco2 id="#7"/>,
-    2: <Disco3 id="#8"/>,
+    0: <Disco1 id="#6" />,
+    1: <Disco2 id="#7" />,
+    2: <Disco3 id="#8" />,
     title: "Disco Machines",
     authorIcon: <BeKind2Robots />,
     authorName: "BeKind2Robots",
@@ -63,34 +63,57 @@ const Collection = ({ type, showModal, setModalPicture }) => {
 
   return (
     <div className={styles.collection_layout}>
-      <div
-        
-        className={styles.big_picture}
-      >
-        <Link href={{pathname: "/nft", query: {type: type, id: collectionType[number] ? collectionType[number].props.id : null}}}>
-        {collectionType[number]}
+      <div className={styles.big_picture}>
+        <Link
+          href={{
+            pathname: "/nft",
+            query: {
+              type: type,
+              id: collectionType[number]
+                ? collectionType[number].props.id
+                : null,
+            },
+          }}
+        >
+          {collectionType[number]}
         </Link>
       </div>
       <div className={styles.small_picture_container}>
         {/* SMALL PICTURES */}
-        <div
-          
-          className={styles.small_picture}
-        >
-          <Link href={{pathname: "/nft", query: {type: type, id: collectionType[number] ? collectionType[pictureNumbers[0]].props.id : null}}}>
-          {collectionType[pictureNumbers[0]]}
+        <div className={styles.small_picture}>
+          <Link
+            href={{
+              pathname: "/nft",
+              query: {
+                type: type,
+                id: collectionType[number]
+                  ? collectionType[pictureNumbers[0]].props.id
+                  : null,
+              },
+            }}
+          >
+            {collectionType[pictureNumbers[0]]}
           </Link>
         </div>
-        <div
-          
-          className={styles.small_picture}
-        >
-          <Link href={{pathname: "/nft", query: {type: type, id: collectionType[number] ? collectionType[pictureNumbers[1]].props.id : null}}}>
-          {collectionType[pictureNumbers[1]]}
+        <div className={styles.small_picture}>
+          <Link
+            href={{
+              pathname: "/nft",
+              query: {
+                type: type,
+                id: collectionType[number]
+                  ? collectionType[pictureNumbers[1]].props.id
+                  : null,
+              },
+            }}
+          >
+            {collectionType[pictureNumbers[1]]}
           </Link>
         </div>
         <div className={styles.small_collection_more}>
-          <h5>1025+</h5>
+          <Link href={{ pathname: "/collection", query: { type: type } }}>
+            <h5>1025+</h5>
+          </Link>
         </div>
       </div>
       <div className={styles.info_container}>
--- components/nft/index.js
+++ components/nft/index.js
@@ -1,3 +1,5 @@
+import { Work_Sans, Space_Mono } from "next/font/google";
+
 import MrFox from "../../styles/avatars/MrFox.svg";
 import Bear from "../../styles/images/animals_collection/bear.svg";
 import Cat from "../../styles/images/animals_collection/cat.svg";
@@ -13,30 +15,49 @@ import Disco1 from "../../styles/images/disco_collection/first_disco.svg";
 import Disco2 from "../../styles/images/disco_collection/second_disco.svg";
 import Disco3 from "../../styles/images/disco_collection/third_disco.svg";
 
+import styles from "./nft.module.css";
+import Link from "next/link";
+
+const workSans = Work_Sans({
+  weight: "300",
+  subsets: ["latin"],
+});
+
+const spaceMono = Space_Mono({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 const collections = [
   {
     type: "animals",
-    0: <Dog id="#0" name="DigiPup Portraits" />,
-    1: <Cat id="#1" name="WhiskerArt Wonders" />,
-    2: <Bear id="#2" name="BearVista Visions" />,
+    nfts: [
+      <Dog key="#0" id="#0" name="DigiPup Portraits" />,
+      <Cat key="#1" id="#1" name="WhiskerArt Wonders" />,
+      <Bear key="#2" id="#2" name="BearVista Visions" />,
+    ],
     title: "DSGN Animals",
     authorIcon: <MrFox />,
     authorName: "MrFox",
   },
   {
     type: "mushrooms",
-    0: <Mushroom1 id="#3" name="Enchanted Spore Serenade" />,
-    1: <Mushroom2 id="#4" name="Fungi Fantasy Forge" />,
-    2: <Mushroom3 id="#5" name="MycoMystic Dreamscapes" />,
+    nfts: [
+      <Mushroom1 key="#3" id="#3" name="Enchanted Spore Serenade" />,
+      <Mushroom2 key="#4" id="#4" name="Fungi Fantasy Forge" />,
+      <Mushroom3 key="#5" id="#5" name="MycoMystic Dreamscapes" />,
+    ],
     title: "Magic Mushrooms",
     authorIcon: <Shroomie />,
     authorName: "Shroomie",
   },
   {
     type: "disco",
-    0: <Disco1 id="#6" name="Mechanical Masterpieces" />,
-    1: <Disco2 id="#7" name="RoboCanvas Realms" />,
-    2: <Disco3 id="#8" name="AI Artifacts Archive" />,
+    nfts: [
+      <Disco1 key="#6" id="#6" name="Mechanical Masterpieces" />,
+      <Disco2 key="#7" id="#7" name="RoboCanvas Realms" />,
+      <Disco3 key="#8" id="#8" name="AI Artifacts Archive" />,
+    ],
     title: "Disco Machines",
     authorIcon: <BeKind2Robots />,
     authorName: "BeKind2Robots",
@@ -45,30 +66,49 @@ const collections = [
 
 // getting NFT based on ID
 
-const Nft = ({ setAuthor, setName, id }) => {
+const Nft = ({ setAuthor, setName, id, type }) => {
   const findComponentById = (id) => {
     for (const collection in collections) {
-      for (const key in collections[collection]) {
+      for (const key in collections[collection].nfts) {
         if (
-          collections[collection][key].props &&
-          collections[collection][key].props.id === id
+          collections[collection].nfts[key].props &&
+          collections[collection].nfts[key].props.id === id
         ) {
-          setName(collections[collection][key].props.name);
+          setName(collections[collection].nfts[key].props.name);
           setAuthor(collections[collection].authorName);
-          return collections[collection][key];
+          return collections[collection].nfts[key];
         }
       }
     }
-    return <div>nerasta</div>;
+    return null;
   };
 
-  const component = findComponentById(id);
+  const findComponentsByType = (type) => {
+    const collectionType = collections.find((item) => item.type === type)
+      ? collections.find((item) => item.type === type)
+      : collections[0];
+    const collection = collectionType.nfts.map((item) => (
+      <Link
+        href={{ pathname: "/nft", query: { type: type, id: item.props.id } }}
+      >
+        <div className={styles.nft_collection_container} key={item.props.id}>
+          {item}
+          <div className={styles.nft_info_title_container}>
+            <p className={workSans.className}>{item.props.name}</p>
+          </div>
+          <div className={styles.nft_info_author_continer}>
+            {collectionType.authorIcon}
+            <p className={workSans.className}>{collectionType.authorName}</p>
+          </div>
+        </div>
+      </Link>
+    ));
+    return collection;
+  };
 
-  return (
-    <>
-      <div>{component}</div>
-    </>
-  );
+  const component = id ? findComponentById(id) : findComponentsByType(type);
+
+  return <>{component}</>;
 };
 
 export default Nft;
